8c6b960b2ba39286e10bee85b4c23435b86ac52a
Jennifer Hightower <jenniferhightower2020@gmail.com>
2023-01-25 13:20:52 -0600

    created methods within signup-contr.classes.php for error handling. methods check for empty input, valid username with regex, and valid email with FILTER_VALIDATE_EMAIL

--- classes/signup-contr.classes.php
+++ classes/signup-contr.classes.php
@@ -1,6 +1,6 @@
 <?php
 
-// this file is for updating database
+// this file is for updating database - controller in MVC model
 
 class SignupContr {
     // protection level: make data inside classes least accessible as needed
@@ -16,6 +16,55 @@ class SignupContr {
         $this->pwd = $pwd;
         $this->pwdRepeat = $pwdRepeat;
         $this->email = $email;
+    }
+
+    // error handling 
+    
+    // method to return true or false if input is filled out
+    private function emptyInput() {
+        $result;
+        // conditional to check if properties inside class are empty or not. properties are assigned to data user submitted
+        if(empty($this->uid) || empty($this->pwd) || empty($this->pwd) || empty($this->pwdRepeat) || empty($this->email)) {
+            $result = false;
+        }  else {
+            $result = true;
+        }
+        return $result;
+
+    }
+
+    // method to check if username is valid username type, check for characters inside username with regex
+    private function invalidUid() {
+        $result;
+        // check username against set of characters
+        if (!preg_match("/^[a-zA-Z0-9]*$/", $this->uid)) {
+            $result = false;
+        } else {
+            $result = true;
+        }
+        return $result;
+    }
+
+    // method to check if email address if correct format
+    private function invalidEmail() {
+        $result;
+        if(!filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+            $result = false;
+        } else {
+            $result = true;
+        }
+        return $result;
+    }
+
+    // method to confirm pwd and pwdRepeat are equal
+    private function pwdMatch() {
+        $result;
+        if($this->pwd !== $this->pwdRepeat) {
+            $result = false;
+        } else {
+            $result = true;
+        }
+        return $result;
     }
 }
 
